abd5e33f3d963353926341aaf10007b84f134d0a
Shmuli Margulies <shmuli9@gmail.com>
2021-02-18 18:19:05 +0000

    added num_styles to route
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-import random
 import time
 
 from flask import Blueprint
@@ -10,8 +9,19 @@ bp = Blueprint("routes", __name__)
 
 @bp.route("/test", methods=["GET", "POST"])
 def test_route():
-    # trigger neural net
-    from app import test
-    test.trigger_nn("app/WarpGAN/data/example/Joker.jpeg", "app/WarpGAN/result")
+    """
+    POST params:
 
-    return {"data": "Success"}
+    :return:
+    """
+    # trigger neural net
+    num_styles = 1
+    from app import test
+    start = time.time()
+    test.trigger_nn("app/WarpGAN/data/example/Joker.jpeg", "app/WarpGAN/result", num_styles)
+
+    return {
+        "data": "Success",
+        "num_styles": num_styles,
+        "time_taken": time.time() - start
+    }
