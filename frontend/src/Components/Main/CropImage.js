82484cd6e72a970aa0f9b524db05b80002349dfd
Shmuli Margulies <shmuli9@gmail.com>
2021-04-26 13:47:35 +0100

    updated to show image even if not customised
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- frontend/src/Components/Main/CropImage.js
+++ frontend/src/Components/Main/CropImage.js
@@ -1,30 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
+import {Figure} from "react-bootstrap";
+import Paper from '@material-ui/core/Paper';
 
 const CropImage = (props) => {
     const {setCropper, image, enabled} = props
 
     return (
         <div className={"row"}>
-            <div className={`col-12 mr-0 ${enabled ? "" : "in"}visible`}>
-                <Cropper
-                    style={{height: 512, width: "100%"}}
-                    src={image}
-                    autoCrop={true}
-                    aspectRatio={1}
-                    viewMode={0}
-                    minCropBoxHeight={256}
-                    minCropBoxWidth={256}
-                    cropBoxResizable={false}
-                    background={true}
-                    responsive={true}
-                    autoCropArea={1}
-                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
-                    onInitialized={(instance) => {
-                        setCropper(instance);
-                    }}
-                />
+            <div className={`col-12 mr-0 `}>
+                <Paper elevation={3}>
+                    {enabled ? <Cropper
+                        style={{height: 512, width: "100%"}}
+                        src={image}
+                        autoCrop={true}
+                        aspectRatio={1}
+                        viewMode={0}
+                        minCropBoxHeight={256}
+                        minCropBoxWidth={256}
+                        cropBoxResizable={false}
+                        background={true}
+                        responsive={true}
+                        autoCropArea={1}
+                        checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                        onInitialized={(instance) => {
+                            setCropper(instance);
+                        }}
+                    /> : <Figure className="mb-0">
+                        <Figure.Image className="mb-0" src={image} alt="image" style={{height: 512}} fluid/>
+                    </Figure>}
+                </Paper>
             </div>
             <br style={{clear: "both"}}/>
         </div>
